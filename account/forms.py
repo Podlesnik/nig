767cec89a533726a98de07e9f8504884eda03085
dmitry <dmitry.latushkov3@teb.edu.pl>
2024-11-05 15:31:13 +0100

    najnowszy kod

--- account/forms.py
+++ account/forms.py
@@ -1,4 +1,20 @@
 from django import forms
+from django.contrib.auth import get_user_model
+
+
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput, label='Haslo')
+    password2 = forms.CharField(widget=forms.PasswordInput, label='Powtorz Haslo')
+    class Meta:
+            model = get_user_model()
+            fields = ('username', 'first_name', 'email')
+
+    def clean_password2(self):
+        if self.cleaned_data['password'] != self.cleaned_data['password2']:
+            raise forms.ValidationError("hasla sa rozne")
+
+        return self.cleaned_data['password2']
